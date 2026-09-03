3f0e9e2f6127444339f5bb946044d9ded1bec093
min <alekdi8gm30@gmail.com>
2026-09-03 12:47:51 +0900

    vision 패키지 코드 작성

--- src/kit_vision/setup.py
+++ src/kit_vision/setup.py
@@ -1,3 +1,6 @@
+from glob import glob
+import os
+
 from setuptools import find_packages, setup
 
 package_name = 'kit_vision'
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'resource'), glob('resource/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +28,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'object_detection = kit_vision.object_detection:main',
         ],
     },
 )
